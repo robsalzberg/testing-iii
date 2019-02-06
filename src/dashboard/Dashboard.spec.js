1535fb98d69d17b449c831320db25477ac7ed607
Rob Salzberg <robsalzberg@gmail.com>
2019-02-05 22:04:31 -0500

    Created Dashboard tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,5 +1,20 @@
 import React from "react";
 import { render, fireEvent } from "react-testing-library";
 import "jest-dom/extend-expect";
+import Dashboard from './Dashboard';
 
-// shows the controls and display
+describe('Dashboard renders without crashing.', () => {
+    it('Renders the Dashboard component', () => {
+        render(<Dashboard />);
+    })
+    it('Renders the Display component', () => {
+        const { getByText } = render(<Dashboard />);
+        getByText(/open/i);
+        getByText(/unlocked/i);
+    })
+    it('Renders the Controls component', () => {
+        const { getByText } = render(<Dashboard />);
+        getByText(/close gate/i);
+        getByText(/lock gate/i);
+    })
+}); 
